9521462a76aadf42d5beff6cf00bf9bbbe9f4dc3
th3w0rst <chomanti.limited@gmail.com>
2022-04-27 21:10:34 +0300

    added country to get products requests

--- ProductConstructor.js
+++ ProductConstructor.js
@@ -1,15 +1,14 @@
 const { getCategoryById } = require("./SQLQuesries/CategorySQL.js");
 const { getSupplierById } = require("./SQLQuesries/SupplierSQL.js");
 
-
 const productConstructor = async (product) => {
-    const supplierName = await getSupplierById(product.supplierid).then(
-        (response) => response.rows[0].suppliername
+    const supplierCountry = await getSupplierById(product.supplierid).then(
+        (response) => response.rows[0].country
     );
     const categoryName = await getCategoryById(product.categoryid).then(
         (response) => response.rows[0].categoryname
     );
-    return { ...product, supplierName, categoryName };
-}
+    return { ...product, supplierCountry, categoryName };
+};
 
 module.exports = productConstructor;
